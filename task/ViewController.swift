fed73321d5bd20ce1bbc37ea66af9ff3f3380848
monish <monishraja0200@gmail.com>
2022-01-13 13:50:10 +0530

    Initial Commit

---
+++ task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task
+//
+//  Created by monish-pt4670 on 23/10/1943 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
